1805956aaf104c05e37544fc0b450ea06b404831
David Henning <david.henning@inovex.de>
2018-06-28 14:35:47 +0200

    Update Node.js modules to latest versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
