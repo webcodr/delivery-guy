afe36d378eedb45a7ee1254dafd835c14af649de
David Henning <david.henning@inovex.de>
2019-04-19 23:49:46 +0200

    Add interceptors and global headers

--- src/delivery_guy.ts
+++ src/delivery_guy.ts
@@ -1,6 +1,53 @@
 class DeliveryGuy {
+  private globalHeaders: object = {}
+
+  private interceptors = {
+    request: [],
+    response: [],
+    error: []
+  }
+
+  public addGlobalHeader(name: string, value: string) {
+    this.globalHeaders[name] = value
+  }
+
+  public intercept(interceptor: string, action: Function) {
+    this.interceptors[interceptor].push(action)
+  }
+
+  private callInterceptorActions(interceptor: string, url: string, payload: RequestInit | Response) {
+    const actions = this.interceptors[interceptor] || []
+
+    if (actions.length > 0) {
+      for (const action of actions) {
+        action(url, payload)
+      }
+    }
+  }
+
+  private createInterceptorPromise(url: string, config: RequestInit): Promise<Response> {
+    let promise: Promise<any> = Promise.resolve()
+
+    promise = promise.then(() => {
+      this.callInterceptorActions('request', url, config)
+    })
+
+    promise = promise.then((): Promise<Response> => fetch(url, config))
+
+    promise = promise.then(
+      (response: Response) => {
+        this.callInterceptorActions('response', url, response)
+
+        return response
+      }
+    )
+
+    return promise
+  }
+
+
   private async request(url: string, body?: BodyInit, userConfig?: RequestInit) {
-    const headers = {}
+    const headers = { ...this.globalHeaders }
 
     if (body && typeof body === 'object') {
       body = JSON.stringify(body)
@@ -13,7 +60,7 @@ class DeliveryGuy {
       headers
     }
 
-    return fetch(url, config)
+    return this.createInterceptorPromise(url, config)
   }
 
   public async get(url: string): Promise<Response> {
--- test/unit/deliver_guy.spec.ts
+++ test/unit/deliver_guy.spec.ts
@@ -3,6 +3,10 @@ import * as fetchMock from 'fetch-mock'
 import * as flushPromises from 'flush-promises'
 
 describe('DeliveryGuy', () => {
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
   describe('get()', () => {
     it('delivers a JSON response', async () => {
       const mockData = { foo: 'bar' }
@@ -14,6 +18,30 @@ describe('DeliveryGuy', () => {
       flushPromises()
 
       expect(jsonBody).toEqual(mockData)
+    })
+  })
+
+  describe('interceptors', () => {
+    it('should call interceptors', async () => {
+      const url = '/foo'
+      const requestInterceptor = jest.fn()
+      const responseInterceptor = jest.fn()
+      DeliveryGuy.intercept('request', requestInterceptor)
+      DeliveryGuy.intercept('response', responseInterceptor)
+      const mockData = { foo: 'bar' }
+
+      fetchMock.get(url, mockData)
+
+      expect(requestInterceptor.mock.calls.length).toBe(0)
+      expect(responseInterceptor.mock.calls.length).toBe(0)
+
+      const response = await DeliveryGuy.get(url)
+
+      expect(await response.json()).toEqual(mockData)
+      expect(requestInterceptor.mock.calls.length).toBe(1)
+      expect(responseInterceptor.mock.calls.length).toBe(1)
+      expect(requestInterceptor.mock.calls[0][0]).toBe(url)
+      expect(responseInterceptor.mock.calls[0][0]).toBe(url)
     })
   })
 })
